a7f0ea928925ed1495b92d903d8a880e3ec2cf95
Danieln416 <daniram01031@gmail.com>
2024-09-08 17:45:43 -0500

    firu es gay x2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,12 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    return this.appService.Firugay();
     return this.appService.getHello();
   }
+}
+
+
+function Firugay(): string {
+  return 'firu es gay!';
 }
